83de753da5ecf1a8321e97a760d0b2aa8c43b09e
Diane Crame <dcrame@icloud.com>
2020-04-03 19:47:18 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,27 @@
+class CashRegister
+  attr_accessor :total, :discount
+  @@items = []
+  
+  def initialize
+    @total = 0
+    self.apply_discount
+  end
+  
+  def add_item(title,price, quantity=1)
+   item_price_total = price * quantity
+  @@items << [title, item_price_total]
+   @total += item_price_total
+  end
 
+  def apply_discount
+    
+  end
+  
+  
+  def items
+  end
+  
+
+  def void_last_transaction
+  end
+end
