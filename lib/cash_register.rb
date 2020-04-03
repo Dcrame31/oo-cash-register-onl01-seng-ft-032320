1cfdb1c2a10f79751081c5d5ea83a0e5c0b715ff
Diane Crame <dcrame@icloud.com>
2020-04-03 21:19:25 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,10 +1,10 @@
 class CashRegister
   attr_accessor :total, :discount
-  @@items = []
   
   def initialize(discount=0)
     @total = 0
-    
+    @discount = discount
+    @cart = []
   end
   
   def add_item(item, price, quantity=1 )
